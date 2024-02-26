f37e41962c493f0e1f03cfa5b780343c07e73ba7
Jey_Slon <jeyllomsandoval@gmail.com>
2024-02-25 20:40:57 -0400

    fix

--- Frontend/astro.config.ts
+++ Frontend/astro.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-import netlify from "@astrojs/netlify";
+import vercelServerless from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: "server",
-  adapter: netlify()
+  output: 'server',
+  adapter: vercelServerless(),
 });
